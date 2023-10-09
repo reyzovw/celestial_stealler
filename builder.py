f7b726640d1afd461ae882b2f62c2d25e53e221b
reyzov <120815160+reyzovw@users.noreply.github.com>
2023-10-09 17:08:47 +0300

    Updated the builder, and fixed some bugs

--- builder.py
+++ builder.py
@@ -1,36 +1,41 @@
 import subprocess
 import os
-import traceback
+from time import sleep
 
-with open(os.devnull, 'w') as fnull:
-    try:
-        from colorama import init, Fore
-    except ModuleNotFoundError:
-        subprocess.call(['pip', 'install', 'colorama'], stdout=fnull, stderr=fnull)
+print("""\033[0;31m
+██████╗    ░██╗░░░██╗   ██╗     ██╗░░░░░    ██████╗     ░███████╗   ██████╗░
+██╔══██╗    ██║░░░██║   ██║     ██║░░░░░    ██╔══██╗    ██╔════╝    ██╔══██╗
+██████╦╝    ██║░░░██║   ██║     ██║░░░░░    ██║░░██║    █████╗░░    ██████╔╝
+██╔══██╗    ██║░░░██║   ██║     ██║░░░░░    ██║░░██║    ██╔══╝░░    ██╔══██╗
+██████╦╝    ╚██████╔╝   ██║     ███████╗    ██████╔╝    ███████╗    ██║░░██║
+╚═════╝░░    ╚═════╝░   ╚═╝     ╚══════╝    ╚═════╝     ░╚══════╝   ╚═╝░░╚═╝
+\033[0m""")
 
-
-print(Fore.RED + """
-            ███████████████████████████████████████████████████████████████████████████████████████████
-            █─▄▄▄▄█─█─█▄─▄▄─█▄─▄▄▀█▄─▄▄─█▄─▄─▀█▄─▄▄─█─▄▄─█─▄▄▄─█─▄─▄─█▄─▄▄─█─█─█▄─▄▄▀█─▄▄─█▄─▄▄─█▄─▄▄─█
-            █▄▄▄▄─█─▄─██─▄█▀██─██─██─▄█▀██─▄─▀██─▄▄▄█─██─█─███▀███─████─▄█▀█─▄─██─██─█─██─██─▄████─▄███
-            ▀▄▄▄▄▄▀▄▀▄▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀▀▀▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▀▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▄▀▀▀▄▄▄▀▀▀
-    """ + Fore.RESET)
+sleep(1)
 
 try:
-    os.system(f'pyinstaller --onefile main.py')
+    os.system("pyinstaller --onefile main.py")
     try:
         import PyInstaller
-    except ImportError:
-        print(f"{Fore.RED}PyInstaller не установлен. Устанавливаю PyInstaller...{Fore.RESET}")
+    except:
+        print('Устанавливаю нужные библиотеки, пожалуйста подождите...')
         with open(os.devnull, 'w') as fnull:
             subprocess.call(['pip', 'install', 'pyinstaller'], stdout=fnull, stderr=fnull)
-        print(f"{Fore.GREEN}Установка PyInstaller завершена{Fore.RESET}")
-    
-    try:
-        os.system(f'pyinstaller --onefile main.py')
-    except Exception as e:
-        print(e)
-        subprocess.call(['-m', 'pip', 'uninstall', 'pathlib'], stdout=fnull, stderr=fnull)
-    print(f'{Fore.GREEN}Файл успешно скомпилирован{Fore.RESET}')
-except Exception as e:
-    print(f'{Fore.RED}Ошибка при компиляции: {e}{Fore.RESET}')
+        print("Библиотеки успешно установленны")
+        try:
+            os.system("pyinstaller --onefile main.py")
+        except Exception as e:
+            print(f'Произошла ошибка: {e}')
+            sleep(1)
+            print('Пожалуйста подождите, программа пытается исправить ее...')
+            with open(os.devnull, 'w') as fnull:
+                subprocess.call(['-m', 'pip', 'uninstall', 'pathlib'], stdout=fnull, stderr=fnull)
+            try:
+                os.system("pyinstaller --onefile main.py")
+                print('Пытаемся собрать файл после попытки исправления ошибка...')
+            except Exception as exp:
+                sleep(1)
+                print(f'Ошибка не исправилась, ошибка: {exp}')
+    print('Сборка успешно завершилась, ваш .exe файл хранится по директории dist/main.py')
+except Exception as tryexp:
+    print(f'Ошибка при запуске: {exp}')
